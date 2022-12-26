027fcdaee3e17a780d664e59530f64f631277dc3
Misha Moroshko <michael.moroshko@gmail.com>
2022-12-26 22:52:38 +1100

    More setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import { Inter } from "@next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <main className={inter.className}>
+    <>
       <h1 className="text-3xl">Beepy</h1>
-    </main>
+    </>
   );
 }
