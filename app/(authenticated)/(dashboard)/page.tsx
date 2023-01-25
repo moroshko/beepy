3a18ef681612bcffc64a11da8f9889f3d5daefa6
Misha Moroshko <michael.moroshko@gmail.com>
2023-01-25 21:48:09 +1100

    Upgrade deps

--- app/(authenticated)/(dashboard)/page.tsx
+++ app/(authenticated)/(dashboard)/page.tsx
@@ -7,20 +7,20 @@ const DashboardPage = async () => {
   const {
     data: { user },
   } = await supabase.auth.getUser();
-  const { data: records, error } = await supabase
+  const { data: initialRecords, error } = await supabase
     .from("records")
     .select("id, sys, dia, pulse, created_at")
     .order("created_at", { ascending: false })
     .limit(10)
     .eq("user_id", user?.id);
 
-  if (records === null) {
+  if (initialRecords === null) {
     throw transformSupabaseError(error);
   }
 
   return (
     <div className="flex justify-center">
-      <Records initialRecords={records} />
+      <Records initialRecords={initialRecords} />
     </div>
   );
 };
