e7ba44bfcfde58827c06210d4025911ff7186e1b
Misha Moroshko <michael.moroshko@gmail.com>
2023-09-30 16:02:11 +1000

    Add console.log

--- app/api/utils.ts
+++ app/api/utils.ts
@@ -2,6 +2,8 @@ import { currentUser } from "@clerk/nextjs";
 import { Pool } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
 
+console.log("====>", process.env.DATABASE_URL);
+
 const client = new Pool({ connectionString: process.env.DATABASE_URL });
 
 export const db = drizzle(client);
